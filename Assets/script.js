700632a25fe71d74ecef276d788481e47f38d887
sourslaw <lawanscott@gmail.com>
2021-04-26 17:07:54 -0700

    updating childnode removal when new city is searched

--- Assets/script.js
+++ Assets/script.js
@@ -12,60 +12,24 @@ const mainF = document.getElementById('mainForecast');
 
 const secondF = document.getElementById('secondaryForecast');
 
-// P all (https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/)
+// promise all (https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/)
 function getApi(requestUrl, requestUrlDos, requestUrlTres) {
 	Promise.all([
 		fetch(requestUrl),
 		fetch(requestUrlDos),
 		fetch(requestUrlTres)
-		// fetch(requestUrlCuatro)
 	]).then(function (responses) {
 			// console.log(responses);
 			return Promise.all(responses.map(function (response) {
 				return response.json();
-				console.log(response);
+				// console.log(response);
 			}));
 		}).then(function (data) {
-			console.log(data);
-
-			// console.log(`temperature: ${data[1].main.temp}`)
-			// console.log(`temperature (feels like): ${data[1].main.feels_like}`)
-			// console.log(`humidity: ${data[1].main.humidity}`)
-			console.log(`windspeed: ${data[1].wind.speed}`)
-
-			// console.log(`weather id: ${data[1].weather[0].id}`)
-			// console.log(`weather main: ${data[1].weather[0].main}`)
-			// console.log(`weather description: ${data[1].weather[0].description}`)
-			// console.log(`weather icon: ${data[1].weather[0].icon}`)
-
-
-			// five day, three hour forcast map
-			// console.log(data[0].list[1].main);
-			console.log(`weather city: ${data[0].city.name}`);
-
-			// console.log(`date: ${data[0].list[0].dt_txt}`);
-
-			// console.log(`temp: ${data[0].list[0].main.temp}`);
-			// console.log(`feels like: ${data[0].list[0].main.feels_like}`);
-			// console.log(`humidity: ${data[0].list[0].main.humidity}`);
-
-			// console.log(`wind speed: ${data[0].list[0].wind.speed}`);
-
-			// console.log(`weather id: ${data[0].list[0].weather[0].id}`);
-			// console.log(`weather main: ${data[0].list[0].weather[0].main}`);
-			// console.log(`weather description: ${data[0].list[0].weather[0].description}`);
-			// console.log(`weather icon: ${data[0].list[0].weather[0].icon}`);
-
-			// lat and lon stuff
-			// console.log(`city: ${data[2].data[0].name}`)
-			// console.log(`latitude: ${data[2].data[0].latitude}`)
-			// console.log(`longitude: ${data[2].data[0].longitude}`)
-
-			// const lat = `${data[2].data[0].latitude}`;
-			// const long = `${data[2].data[0].longitude}`;
-			console.log(data[2].data[0].latitude);
-			console.log(data[2].data[0].longitude);
-
+			// console.log(data);
+			// console.log(`windspeed: ${data[1].wind.speed}`)
+			// console.log(`weather city: ${data[0].city.name}`);
+			// console.log(data[2].data[0].latitude);
+			// console.log(data[2].data[0].longitude);
 			const lat = `${data[2].data[0].latitude}`
 			const long = `${data[2].data[0].longitude}`
 
@@ -82,44 +46,60 @@ function getApi(requestUrl, requestUrlDos, requestUrlTres) {
 			console.log(`uvi index: ${newData.current.uvi}`)
 
 			addMain(newData);
-		});
 
-			
-		// });
+		});
 };
+
 // form
 function handleForm(event) {
 	event.preventDefault();
 	console.log(`form submitted, search value: ${searchInput.value}`);
-	// const requestUrl = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
 
 	const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&units=imperial&appid=${apiKey}`;
-
 	const requestUrlDos = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=imperial&appid=${apiKey}`;
-
 	const requestUrlTres = `http://api.positionstack.com/v1/forward?access_key=${psApiKey}&country=us&query=${searchInput.value}`;
-	
-	// const requestUrlCuatro = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}`;
 	
 	getApi(requestUrl, requestUrlDos, requestUrlTres);
 
-	// save search to drop down menu
+	// save recent search to drop down menu
 	const pastSearch = document.createElement('option');
 	pastSearch.setAttribute('value', `${searchInput.value}`);
 	pastSearch.innerText = `${searchInput.value}`;
 
 	selection.append(pastSearch);
 
+	// remove child node on new search
+	const byeMain = document.getElementById('mainForecast');
+	while (byeMain.firstChild) {
+		byeMain.removeChild(byeMain.firstChild);
+	  }
+	const byeForecast = document.getElementById('secondaryForecast');
+	while (byeForecast.firstChild) {
+		byeForecast.removeChild(byeForecast.firstChild);
+	  }
 };
 
-// dropdown
+// dropdown form handler (maybe combine with main form via input.value)
 function handleOtherForm(event) {
 	event.preventDefault();
+
 	console.log(`OTHER form submitted, search value: ${searchInput.value}`);
+
 	const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${selection.value}&units=imperial&appid=${apiKey}`;
 	const requestUrlDos = `https://api.openweathermap.org/data/2.5/weather?q=${selection.value}&units=imperial&appid=${apiKey}`
 	const requestUrlTres = `http://api.positionstack.com/v1/forward?access_key=${psApiKey}&country=us&query=${selection.value}`;
+	
 	getApi(requestUrl, requestUrlDos, requestUrlTres);
+
+	// remove child node on new search TWO (make this a function?)
+	const byeMain = document.getElementById('mainForecast');
+	while (byeMain.firstChild) {
+		byeMain.removeChild(byeMain.firstChild);
+		}
+	const byeForecast = document.getElementById('secondaryForecast');
+	while (byeForecast.firstChild) {
+		byeForecast.removeChild(byeForecast.firstChild);
+		}
 };
 
 // search field
@@ -128,11 +108,19 @@ searchForm.addEventListener('submit', handleForm);
 selection.addEventListener('change', handleOtherForm);
 
 // faker maincard
+const pFour = document.createElement('p');
+
 function addMain(newData){
+
 	console.log("im outside the big call")
-} 
+	// const pFour = document.createElement('p');
+	// pFour.setAttribute('id', 'uvIndex');
+	// cardBody.append(pFour);
+	pFour.innerText = `uvi index: ${newData.current.uvi}`;
 
+}; 
 
+// five day forecast function
 function forecastCards(data) {
 	
 	for (i = 0; i < (data[0].list).length; i +=8) {
@@ -165,29 +153,27 @@ function forecastCards(data) {
 	}
 };
 
+// day function for mainforecast
 function todayIs() {
 	const d = new Date();
 	const weekday = ['sun', 'mon', 'tues', 'weds', 'thurs', 'fri', 'sat'];
 	let today = weekday[d.getDay()];
 	// console.log(today)
 	return today;
-}
+};
 
+// main (today) forecast function
 function mainCard(data) {
 	
 	const cardContainer = document.createElement('div');
 	cardContainer.className = 'card';
 	cardContainer.setAttribute('style', 'width: 25rem;');
-
 	
 	const cardBody = document.createElement('div');
 	cardBody.className = 'card-body';
 	const hFourer = document.createElement('h4');
 	hFourer.innerText = `${data[0].city.name}`;
 	const hFiver = document.createElement('h5');
-
-	// const locDate = (`${data[0].list[i].dt_txt}`);
-	// hFiver.innerText = locDate.slice(0,10);
 	hFiver.innerText = (todayIs());
 
 	// weather icons (https://openweathermap.org/weather-conditions)
@@ -214,12 +200,16 @@ function mainCard(data) {
 	pThree.setAttribute('id', 'windSpeed');
 	pThree.innerText = 'windspeed: ' + `${data[1].wind.speed}` + 'mph';
 	cardBody.append(pThree)
+	// const pFour = document.createElement('p');
+	pFour.setAttribute('id', 'uvIndex');
+	cardBody.append(pFour);
 
 	cardContainer.append(cardBody)
 
 	mainForecast.append(cardContainer);
 };
 
+// icons array
 const icons = {
 	'01d': 'Assets/Images/01d.svg',
 	'02d': 'Assets/Images/02d.svg',
